e9417f3bc52a7e20dff81a3109cab8a66d1a9821
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-03 23:28:14 -0600

    selecting and manipulating elements

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 'use strict';
-//
-document.querySelector('.message');
+
+//? selecting and manipulating elements
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+//? to get value property use .value
+document.querySelector('.guess').value = 22;
+console.log(document.querySelector('.guess').value);
