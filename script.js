a7e3f7f65cfe7462d897cf730f34c611eb80d556
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-06 21:04:46 -0600

    handeling click events

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 'use strict';
 
-//? selecting and manipulating elements
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// //? selecting and manipulating elements
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-//? to get value property use .value
-document.querySelector('.guess').value = 22;
-console.log(document.querySelector('.guess').value);
+// //? to get value property use .value
+// document.querySelector('.guess').value = 22;
+// console.log(document.querySelector('.guess').value);
+
+//? handeling click events
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else {
+    document.querySelector('.message').textContent = 'Number!';
+  }
+});
