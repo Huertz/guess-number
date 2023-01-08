8c3a21173aaa8fc991d86646024e809a3a5ff144
Huertz <104065687+Huertz@users.noreply.github.com>
2023-01-07 19:15:58 -0600

    implementing game logic and css changes

--- script.js
+++ script.js
@@ -11,14 +11,47 @@
 // document.querySelector('.guess').value = 22;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 //? handeling click events
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  // when players is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
-  } else {
-    document.querySelector('.message').textContent = 'Number!';
+    // when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    // when guess is to high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too  high!';
+      //score = score - 1
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' ❌ You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+      document.querySelector('.number').style.width = '30rem';
+    }
+    // when guess is to low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too  low!';
+      //score = score - 1
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' ❌ You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+      document.querySelector('.number').style.width = '30rem';
+    }
   }
 });
